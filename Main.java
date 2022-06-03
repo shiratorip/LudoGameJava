63727ddf385ef9bbce95182c3fe4412b7a5ee98f
Kiryl <102504342+bulchonok@users.noreply.github.com>
2022-06-03 07:17:17 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,91 +1,164 @@
+import javax.crypto.spec.PSource;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     static char[][] field = new char[13][27];
-    static int[][] path=new int[40][2];
+    static int[][] path = new int[40][2];
+
+    static final Scanner sc = new Scanner(System.in);
+    static int[][] pawns = new int[4][4];
+    static int turnCounter = 0;
+    static boolean gameEnd = false;
+    static int PlayersNum;
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int PlayersNum = sc.nextInt();
+
+        PlayersNum = sc.nextInt();
+
         fieldstdCreate();
         fillPath();
         addPlayers(PlayersNum);
         addPawnsToField(PlayersNum);
+
         printField();
-        char plop='-';
-        System.out.println();
-        for (int i = 0; i < 13; i++) {
-            for (int j = 0; j < 27; j++) {
-                for (int k = 0; k < 40; k++) {
+        while (!gameEnd) {
+            playTurn();
+            printField();
 
-                    if (path[k][0]==j &&path[k][1]==i){
-                        plop='o';
-                        if (k==0)plop='P';
-                    }
-                }System.out.print(plop);
-                plop='-';
-            }
-            System.out.println();
         }
-
     }
-    public static void move(){
+
+    public static void playTurn() {
+        System.out.println((char) ((turnCounter % PlayersNum) + 'a') + "\'s turn");
+        for (int i = 0; i < pawns.length; i++) {
+            System.out.println(" "+pawns[turnCounter %4][i]);
+        }
+        System.out.println("enter pawn's coordinate(-1 - move pawn from home):");
+        int coordinateIn = sc.nextInt();
+        boolean checker = false;
+        int rndm = ((int) (Math.random() * 6) + 1);
+        if (coordinateIn >= -1 && coordinateIn <= 39) {
+            if (coordinateIn == -1) {
+                if (rndm == 6) {
+                   //field[path[(turnCounter % PlayersNum) * 10][1]][path[(turnCounter % PlayersNum) * 10][0]] = (char) ((turnCounter % PlayersNum) + 'a');
+                   //for (int i = 0; i <pawns[turnCounter %4].length ; i++) {
+                   //        if (pawns[turnCounter %4][i]==-2){
+                   //        pawns[turnCounter %4][i]=((turnCounter % PlayersNum) * 10);
+                   //            for (int l = 0; l < pawns.length; l++) {
+                   //                System.out.println(" "+pawns[turnCounter %4][l]);
+                   //            }
+                   //        break;
+                   //        }
+                   //}
+
+                    for (int i = 0; i < PlayersNum; i++) {
+                        for (int j = 0; j <pawns[i].length ; j++) {
+                            if (pawns[i][j]==-2){
+                                pawns[i][j]=i*10;
+                                break;
+                            }
+                        }
+
+                    }
+
+                    System.out.println("pawn moved");
+                } else {
+                    System.out.println("your roll is: \'" + rndm + "\'");
+
+                }
+            } else {
+                for (int i = 0; i < pawns[turnCounter % PlayersNum].length; i++) {
+
+                    if (pawns[turnCounter % PlayersNum][i] == coordinateIn) {
+                        checker = true;
+                        field[path[(turnCounter % PlayersNum) * 10][1]][path[(turnCounter % 4) * 10][0]] = 'p';
+                        //
+                        pawns[turnCounter % PlayersNum][i] = move(coordinateIn);
+                        field[path[pawns[turnCounter % PlayersNum][i]][1]][path[pawns[turnCounter % PlayersNum][i]][0]] = (char) ((turnCounter % PlayersNum) + 'a');
+
+                    }
+                }
+
+                if (!checker) {
+                    System.out.println("ERROR: no suitable pawn");
+                    playTurn();
+                }
+
+            }
+        }else {
+            System.out.println("ERROR: wrong input");
+            playTurn();
+        }
+        //if ()
+        //boolean pathBlocked;
+//
+        //if (finishCell)
+//
+        System.out.println(turnCounter);
+        turnCounter++;
+    }
+
+    public static int move(int coordinate) {
+        int random = (int) (Math.random() * 6) + 1;
+        System.out.println("your roll is: \'" + random + "\'");
+        //if ()
+        return coordinate + random;
 
     }
 
     private static void fillPath() {
-        int number=0;
+        int number = 0;
         //
         for (int i = 0; i < 4; i++) {
-            path[number]= new int[]{15, 2+i};
+            path[number] = new int[]{15, 1 + i};
             number++;
         }
         for (int i = 0; i < 4; i++) {
-            path[number]= new int[]{17+(2*i), 5};
+            path[number] = new int[]{15 + (2 * i), 5};
             number++;
         }
         for (int i = 0; i < 2; i++) {
-            path[number]= new int[]{23, 6+i};
-            number++;
-        }
-        //
-        for (int i = 0; i < 4; i++) {
-            path[number]= new int[]{21-(2*i), 7};
-            number++;
-        }
-        for (int i = 0; i < 4; i++) {
-            path[number]= new int[]{15, 8+i};
-            number++;
-        }
-        for (int i = 0; i < 2; i++) {
-            path[number]= new int[]{13-(2*i), 11};
+            path[number] = new int[]{23, 5 + i};
             number++;
         }
         //
         for (int i = 0; i < 4; i++) {
-            path[number]= new int[]{11, 10-i};
+            path[number] = new int[]{23 - (2 * i), 7};
             number++;
         }
         for (int i = 0; i < 4; i++) {
-            path[number]= new int[]{9-(2*i), 7};
+            path[number] = new int[]{15, 7 + i};
             number++;
         }
         for (int i = 0; i < 2; i++) {
-            path[number]= new int[]{3, 6-i};
+            path[number] = new int[]{15 - (2 * i), 11};
             number++;
         }
         //
         for (int i = 0; i < 4; i++) {
-            path[number]= new int[]{5+(2*i),5};
+            path[number] = new int[]{11, 11 - i};
             number++;
         }
         for (int i = 0; i < 4; i++) {
-            path[number]= new int[]{11, 4-i};
+            path[number] = new int[]{11 - (2 * i), 7};
             number++;
         }
         for (int i = 0; i < 2; i++) {
-            path[number]= new int[]{13+(2*i), 1};
+            path[number] = new int[]{3, 7 - i};
+            number++;
+        }
+        //
+        for (int i = 0; i < 4; i++) {
+            path[number] = new int[]{3 + (2 * i), 5};
+            number++;
+        }
+        for (int i = 0; i < 4; i++) {
+            path[number] = new int[]{11, 5 - i};
+            number++;
+        }
+        for (int i = 0; i < 2; i++) {
+            path[number] = new int[]{11 + (2 * i), 1};
             number++;
         }
     }
@@ -145,33 +218,36 @@ public class Main {
     }
 
     static void addPawnsToField(int PlayersNum) {
-        field[1][3] = 'a';
-        field[1][5] = 'a';
-        field[2][3] = 'a';
-        field[2][5] = 'a';
+        for (int i = 0; i < pawns.length; i++) {
+            for (int j = 0; j < pawns[i].length; j++) {
+                pawns[i][j] = -2;
+            }
+        }
+
+        field[1][23] = 'a';
+        field[1][21] = 'a';
+        field[2][23] = 'a';
+        field[2][21] = 'a';
         //
-        field[1][23] = 'b';
-        field[1][21] = 'b';
-        field[2][23] = 'b';
-        field[2][21] = 'b';
-        //
-        if (PlayersNum>2){
+        field[10][23] = 'b';
+        field[10][21] = 'b';
+        field[11][23] = 'b';
+        field[11][21] = 'b';
+        if (PlayersNum > 2) {
             field[10][3] = 'c';
             field[10][5] = 'c';
             field[11][3] = 'c';
             field[11][5] = 'c';
-            //
-            if (PlayersNum>3){
-                field[10][23] = 'd';
-                field[10][21] = 'd';
-                field[11][23] = 'd';
-                field[11][21] = 'd';
+            if (PlayersNum > 3) {
+                field[1][3] = 'd';
+                field[1][5] = 'd';
+                field[2][3] = 'd';
+                field[2][5] = 'd';
             }
         }
     }
 
 
-
-    }
+}
 
 
